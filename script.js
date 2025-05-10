43bd890730c2457fb4f2927cb8b444987d0fe8ca
Tanya <Tanya1Suraj143@gmail.com>
2025-05-10 16:16:41 +0530

    mywebpage

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+  const message = document.getElementById("message");
+  message.textContent = "This message was added using JavaScript!";
+});
